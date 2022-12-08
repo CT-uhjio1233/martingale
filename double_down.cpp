39b1f4c01d1c691d5c5158ddcd810ba31e546416
CT <60270898+CT-uhjio1233@users.noreply.github.com>
2022-12-08 22:09:19 +0800

    double_down.cpp
    雙骰子選大小，相加小於7為小，大於(包含)7為大。
    一開始輸入本金，再來選擇下注的基礎倍率，倍率設定為2的次方。
    每次循環選擇大(b)或小(s)，數字正確時獲得下注的2倍金額，數字錯誤時扣除下注金額，下回合下注金額為本回合的2倍。
    只要本金還有就能一直下注直到破產。

---
+++ double_down.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+using namespace std;
+
+string dice(){
+  srand( time(NULL) );
+  /* 指定亂數範圍 */
+  int min = 1;
+  int max = 6;
+
+  /* 產生 [min , max) 的浮點數亂數 */
+  int n1 = rand() % (max - min + 1) + min;
+  int n2 = rand() % (max - min + 1) + min;
+  string x;
+  if(n1+n2<=6) x="s";
+  else x="b";
+
+  return x;
+}
+int main(){
+  srand( time(NULL) );
+  int money,time=1,lose=1,r,count=0,continuous=0;
+  string choose,n;
+  cout<<"輸入本金:";
+  cin>>money;
+  cout<<"選擇下注基礎倍率(2的次方):";
+  cin>>r;
+  time=r;
+  /* 從1元開始玩輸了加倍下注直到贏在從頭開始 */
+  while(money>0){
+    count++;
+    n=dice();
+    int down;
+    if (money>=pow(2,time))
+    {
+      down=pow(2,time);
+      cout<<"下注:"<<pow(2,time)<<",餘額:"<<money-down;
+    }else{
+      down=money;
+      cout<<"下注:"<<money<<",餘額:"<<money-down;
+    }
+    
+    
+    /* 輸入b or s (b=big,s=small)*/
+    cout<<"\n輸入b or s(選大小):";
+    cin>>choose;
+    /* 勝或敗*/
+    if(choose!=n){
+        lose=1;
+    }
+    else{
+        lose=0;
+    }
+
+    if(lose){
+       continuous++;
+        cout<<"連輸"<<continuous<<"次,扣除"<<down;
+        money-=down;
+        time=time+1;
+    }else{
+        continuous=0;
+        cout<<"贏,獲得"<<down*2;
+        money+=down;
+        time=r;
+    }
+    cout<<",餘額:"<<money<<endl;
+  }
+  cout<<"總下注次數"<<count<<"\n餘額:"<<money<<endl;
+  
+  return 0;
+}
